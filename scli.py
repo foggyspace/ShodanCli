5ea6a17d108fe2220157ea65117428b2f135cbb4
seaung <seaung.c0m7de@protonmail.com>
2019-08-12 22:10:58 +0800

    测试

--- scli.py
+++ scli.py
@@ -6,16 +6,32 @@ __version__ = '0.0.1'
 __author__ = 'seaung'
 
 
+# colors
+W = '\033[0m'
+G = '\033[1;32m'
+R = '\033[1;31m'
+O = '\033[1;33m'
+B = '\033[1;34m'
+
+
+def print_help():
+    helps = '''
+            >>> python scli.py -ip 192.168.2.101
+            >>> python scli.py -host www.baidu.com
+            >>> python scli.py -search nginx
+    '''
+
+
 def banner():
-    print(f'''
-          version : {__version__}
-          author  : {__author__}
-               _               _                    _ _
+    print(G + '''
+          version : {}
+          author  : {}
+     _               _                    _ _
  ___| |__   ___   __| | __ _ _ __     ___| (_)
 / __| '_ \ / _ \ / _` |/ _` | '_ \   / __| | |
 \__ \ | | | (_) | (_| | (_| | | | | | (__| | |
 |___/_| |_|\___/ \__,_|\__,_|_| |_|  \___|_|_|
-          ''')
+          '''.format(__version__, __author__) + R)
 
 
 def main():
@@ -23,23 +39,25 @@ def main():
     主调函数
     :return: None
     '''
-    add_parser_argument = argparse.ArgumentParser()
+    try:
+        add_parser_argument = argparse.ArgumentParser()
 
-    add_parser_argument.add_argument('-ip', dest='ip', type=str, action='store')
-    add_parser_argument.add_argument('-host', dest='host', type=str, action='store')
-    add_parser_argument.add_argument('-search', dest='search', type=str, action='store')
+        add_parser_argument.add_argument('-ip', dest='ip', type=str, action='store')
+        add_parser_argument.add_argument('-host', dest='host', type=str, action='store')
+        add_parser_argument.add_argument('-search', dest='search', type=str, action='store')
 
-    parser_args = add_parser_argument.parser_args()
+        parser_args = add_parser_argument.parse_args()
 
-    if parser_args.ip is None or parser_args.host is None or parser_args.search is None:
-        parser_args.print_help()
-        parser_args.print_usage()
-    elif parser_args.ip:
-        parse_shodan_host_banner_info(parser_args.ip)
-    elif parser_args.host:
-        lookup_a_host(parser_args.host)
-    elif parser_args.search:
-        searching_shodan(parser_args.search)
+        if parser_args.ip is None and parser_args.host is None and parser_args.search is None:
+            print_help()
+        elif parser_args.ip:
+            parse_shodan_host_banner_info(parser_args.ip)
+        elif parser_args.host:
+            lookup_a_host(parser_args.host)
+        elif parser_args.search:
+            searching_shodan(parser_args.search)
+    except KeyboardInterrupt:
+        exit()
 
 
 if __name__ == '__main__':
